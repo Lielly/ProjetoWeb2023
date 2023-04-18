7d44545526822e1691be03f28084b73557500b33
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-04-18 12:52:52 -0300

    Inclusão do create de fabricantes

--- WebAppProjeto01/Context/EFContext.cs
+++ WebAppProjeto01/Context/EFContext.cs
@@ -6,13 +6,11 @@ using System.Web;
 
 namespace WebAppProjeto01.Models
 {
-    public class EFContext
-    {
         public class EFContext : DbContext
         {
             public EFContext() : base("Asp_Net_MVC_CS") { }
             public DbSet<Categoria> Categorias { get; set; }
             public DbSet<Fabricante> Fabricantes { get; set; }
         }
-    }
+   
 }
