4b203eb7dec7c35400a9d4da3b318f801dd7266b
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-05-08 14:26:13 -0300

    Finalização layout do CRUD de Fabricantes

--- WebAppProjeto01/Controllers/FabricantesController.cs
+++ WebAppProjeto01/Controllers/FabricantesController.cs
@@ -102,6 +102,7 @@ namespace WebAppProjeto01.Controllers
             Fabricante fabricante = context.Fabricantes.Find(id);
             context.Fabricantes.Remove(fabricante);
             context.SaveChanges();
+            TempData["Message"] = "Fabricante \"" + fabricante.Nome.ToUpper() + "\" foi removido";
             return RedirectToAction("Index");
         }
     }
