e039ac3b8ead8cf94c83a61c61caab716eef64cc
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-06-05 14:20:28 -0300

    Início cap04

--- WebAppProjeto01/Context/EFContext.cs
+++ WebAppProjeto01/Context/EFContext.cs
@@ -8,9 +8,14 @@ namespace WebAppProjeto01.Models
 {
         public class EFContext : DbContext
         {
-            public EFContext() : base("Asp_Net_MVC_CS") { }
-            public DbSet<Categoria> Categorias { get; set; }
-            public DbSet<Fabricante> Fabricantes { get; set; }
+        public EFContext() : base("Asp_Net_MVC_CS")
+        {
+            Database.SetInitializer<EFContext>(
+            new DropCreateDatabaseIfModelChanges<EFContext>());
         }
+        public DbSet<Categoria> Categorias { get; set; }
+            public DbSet<Fabricante> Fabricantes { get; set; }
+            public DbSet<Produto> Produtos { get; set; }
+    }
    
 }
--- WebAppProjeto01/Models/Fabricante.cs
+++ WebAppProjeto01/Models/Fabricante.cs
@@ -9,5 +9,6 @@ namespace WebAppProjeto01.Models
     {
         public long FabricanteId { get; set; }
         public string Nome { get; set; }
+        public virtual ICollection<Produto> Produtos { get; set; }
     }
 }
