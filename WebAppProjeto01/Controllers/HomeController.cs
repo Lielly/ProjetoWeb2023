724f5aa0fdb37c53a7720927ce5284717e4a631d
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-06-13 15:56:33 -0300

    inicio lista4-cap4

--- WebAppProjeto01/Controllers/HomeController.cs
+++ WebAppProjeto01/Controllers/HomeController.cs
@@ -20,4 +20,39 @@ namespace WebAppProjeto01.Controllers
             return View(h);
         }
     }
+    public ActionResult Index(long? FabricanteId, long? CategoriaId)
+    {
+        Home h = new Home();
+        h.fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+        h.categorias = context.Categorias.OrderBy(c => c.Nome);
+        if((FabricanteId != null))
+        {
+            h.filtro = "Fabricante";
+            h.produtos = context.Produtos.Where(p => p.FabricanteId == FabricanteId).OrderBy(c => c.Nome);
+        }
+        if ((CategoriaId != null))
+        {
+            h.filtro = "Categoria";
+            h.produtos = context.Produtos.Where(p => p.CategoriaId == CategoriaId).OrderBy(c => c.Nome);
+        }
+        return View(h);
+    }
+    public ActionResult IndexProdutosFabricante(long FabId)
+    {
+        Home h = new Home();
+        h.fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+        h.categorias = context.Categorias.OrderBy(c => c.Nome);
+        h.produtos = context.Produtos.Where(p => p.FabricanteId == FabId).OrderBy(c => c.Nome);
+
+        return View(h);
+    }
+    public ActionResult IndexProdutosCategoria(long CatId)
+    {
+        Home h = new Home();
+        h.fabricantes = context.Fabricantes.OrderBy(c => c.Nome);
+        h.categorias = context.Categorias.OrderBy(c => c.Nome);
+        h.produtos = context.Produtos.Where(p => p.CategoriaeId == CatId).OrderBy(c => c.Nome);
+
+        return View(h);
+    }
 }
