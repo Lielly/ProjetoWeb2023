d50eb7df4f15b7ebe3eac6120192f8a3bd8d83fa
Lielly Nátally <lielly.natally@academico.ifrn.edu.br>
2023-07-10 14:09:50 -0300

    cap05 - inicio

--- Modelo1/Tabelas/Categoria.cs
+++ Modelo1/Tabelas/Categoria.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Modelo.Cadastros;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
 namespace WebAppProjeto01.Models
 {
-    public class Categoria
+    public class Modelo.Tabelas
     {
         public long CategoriaId { get; set; }
         public string Nome { get; set; }
